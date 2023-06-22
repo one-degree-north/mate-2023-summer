8c1c0374d13cc678c872308161239d26102bf5f8
chickwensrule <80529810+ChickwensRule@users.noreply.github.com>
2023-06-22 12:37:47 +0800

    fix: switch from json to yaml

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from PyQt6.QtWidgets import QApplication
 
 import yaml
-import pprint
 
 from gui.gui import MainWindow
 
 if __name__ == "__main__":
-    with open("settings.json") as f:
+    with open("settings.yaml") as f:
         settings = yaml.safe_load(f)
     print(yaml.safe_dump(settings))
     print("Modify settings in 'settings.yaml'")
